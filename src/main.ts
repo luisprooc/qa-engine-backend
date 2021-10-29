5cc5c4e95c52da83b4e50dac920196f16dc979fc
Luis Rosario <luisprooc@gmail.com>
2021-10-29 14:18:05 -0400

    feat: add swagger module

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,23 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as helmet from 'helmet';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({});
   app.use(helmet());
   app.setGlobalPrefix('api/v1');
+
+  const config = new DocumentBuilder()
+    .setTitle('QA Engine API')
+    .setDescription('The QA API description')
+    .setVersion('1.0')
+    .addTag('QA')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, document);
+
   await app.listen(AppModule.port);
 }
 bootstrap();
