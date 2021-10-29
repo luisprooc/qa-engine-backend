e2bc81dd5c9e10f4a5d75636477185161ff94d8d
Luis Rosario <luisprooc@gmail.com>
2021-10-29 18:16:03 -0400

    feat: add docs to create-user dto

--- src/models/users/dto/create-user.dto.ts
+++ src/models/users/dto/create-user.dto.ts
@@ -1,26 +1,36 @@
 import { IsString, IsInt, IsBoolean, IsDate } from 'class-validator';
+import {ApiProperty } from '@nestjs/swagger';
 export class CreateUserDto {
+
+  @ApiProperty({ nullable: false,  readOnly: true, description: 'It\'s generated automatically' })
   @IsInt()
   id: number;
 
+  @ApiProperty({ nullable: false,  example: 'example@gmail.com' })
   @IsString()
   email: string;
 
+  @ApiProperty({ nullable: false, minLength: 8 })
   @IsString()
   password: string;
 
+  @ApiProperty({ nullable: false, maxLength: 60, example: 'Jhon Doe' })
   @IsString()
   fullName: string;
 
+  @ApiProperty({ nullable: true })
   @IsInt()
   age?: number;
 
+  @ApiProperty({ nullable: false, default: true })
   @IsBoolean()
-  isActive: number;
+  isActive: boolean;
 
+  @ApiProperty({ nullable: false, description: 'It\'s generated automatically', readOnly: true })
   @IsDate()
   createdDate: Date;
 
+  @ApiProperty({ nullable: false, description: 'It\'s generated automatically', readOnly: true })
   @IsDate()
   updatedDate: Date;
 }
