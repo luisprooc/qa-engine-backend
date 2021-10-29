0797fea72a98b736b36550b4f255e66c71e08eb5
Luis Rosario <luisprooc@gmail.com>
2021-10-29 17:49:21 -0400

    feat: add create userDTO

--- src/models/users/dto/create-user.dto.ts
+++ src/models/users/dto/create-user.dto.ts
@@ -1,1 +1,26 @@
-export class CreateUserDto {}
+import { IsString, IsInt, IsBoolean, IsDate } from 'class-validator';
+export class CreateUserDto {
+  @IsInt()
+  id: number;
+
+  @IsString()
+  email: string;
+
+  @IsString()
+  password: string;
+
+  @IsString()
+  fullName: string;
+
+  @IsInt()
+  age?: number;
+
+  @IsBoolean()
+  isActive: number;
+
+  @IsDate()
+  createdDate: Date;
+
+  @IsDate()
+  updatedDate: Date;
+}
