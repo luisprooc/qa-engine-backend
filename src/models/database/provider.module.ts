cafb7f226cc9b11ed67691ebc8a27b67131b4adc
Luis Rosario <luisprooc@gmail.com>
2021-11-10 18:52:38 -0400

    refactoring(module): ref in database provider module

--- src/models/database/provider.module.ts
+++ src/models/database/provider.module.ts
@@ -7,21 +7,26 @@ import { PostgresConfigService } from 'src/config/database/postgres/config.servi
   imports: [
     TypeOrmModule.forRootAsync({
       imports: [PostgresConfigModule],
+      inject: [PostgresConfigService],
       useFactory: async (_postgresConfigService: PostgresConfigService) => {
+
+        const defaultConnections = {
+          type: 'postgres' as DatabaseType,
+          entities: [__dirname + '/../**/**/entitities/*.entity{.ts,.js}'],
+          autoLoadEntities: true,
+        }
         
         if (process.env.NODE_ENV === 'production') {
           const parse = require('pg-connection-string').parse;
           const config = parse(process.env.DATABASE_URL);
 
           return {
-            type: 'postgres' as DatabaseType,
+            ...defaultConnections,
             host: config.host,
             port: config.port,
             username: config.user,
             password: config.password,
             database: config.database,
-            entities: ['/src/models/**/**/*.entity{.ts,.js}'],
-            autoLoadEntities: true,
             ssl: {
               rejectUnauthorized: false
             },
@@ -32,19 +37,16 @@ import { PostgresConfigService } from 'src/config/database/postgres/config.servi
         }
 
         return {
-          type: 'postgres' as DatabaseType,
+          ...defaultConnections,
           host: _postgresConfigService.host,
           port: _postgresConfigService.port,
           username: _postgresConfigService.username,
           password: _postgresConfigService.password,
           database: _postgresConfigService.database,
-          entities: [__dirname + '/../**/**/entitities/*.entity{.ts,.js}'],
-          autoLoadEntities: true,
           synchronize: true,
           migrationsRun: true,
         }
       },
-      inject: [PostgresConfigService],
     } as TypeOrmModuleAsyncOptions),
   ],
 })
